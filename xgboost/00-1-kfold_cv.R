13eaf1700a3cc6ed57ca3629a69432a0a57a5035
FranziskaSW <franziska.wehrmann@gmail.com>
2018-03-11 20:22:30 +0200

    xgboost cv and tuning

---
+++ xgboost/00-1-kfold_cv.R
@@ -0,0 +1,77 @@
+#----------------------------------------------------------------------------------
+# performs k-fold cross validation
+#----------------------------------------------------------------------------------
+# input   : tr.v (training set where tau-category == v (v in 1:6))
+# output  : measure (AUC, loss, which tau) of the combined cross validation 
+#----------------------------------------------------------------------------------
+library(caret)
+library(pROC)
+#library(doParallel)
+#library(microbenchmark)
+library(data.table)
+library(mlr)
+
+# k-fold cross validation
+k <- k              # is choosen in parent script
+sample.idx <- sample(nrow(tr.v))
+train.rnd  <- tr.v[sample.idx,] # randomised tr.v (same rows but random order)
+folds <- cut(1:nrow(train.rnd), breaks = k, labels = FALSE)
+
+vec <- list()
+results.par <- list()
+
+for (n in 1:nrow(parameters)){
+  vec.1 <- results.par
+  for (i in 1:k){
+    gc()
+    set.seed(1234)
+    # Split data into training and validation
+    idx.val  <- which(folds == i, arr.ind = TRUE)
+    cv.train <- train.rnd[-idx.val,]
+    cv.train <- cv.train[order(cv.train$order_item_id),]
+    cv.val   <- train.rnd[idx.val,]
+    cv.val   <- cv.val[order(cv.val$order_item_id),]
+    cv.train$return <- as.factor(cv.train$return)
+    task <- makeClassifTask(data = cv.train, target = "return", positive = "1")
+    
+    
+    xgb.learner <- makeLearner("classif.xgboost", predict.type = "prob", 
+                               par.vals = list("verbose" = 1)) 
+    
+    #set tuning parameters
+    par.vals <- list("nrounds"   = parameters$nrounds[n], 
+                     "max_depth" = parameters$max_depth[n], 
+                     "eta"       = parameters$eta[n], 
+                     "gamma"     = parameters$gamma[n], 
+                     "colsample_bytree" = parameters$colsample_bytree[n], 
+                     "min_child_weight" = parameters$min_child_weight[n])
+    
+    xgb.learner <- setHyperPars(xgb.learner, par.vals = par.vals, "verbose" = 0)
+    # train xgboost and make prediction 
+    xgb <- mlr::train(xgb.learner, task = task)
+    yhat <- predict(xgb, newdata = cv.val)
+    yhat.val <- yhat[2]$data$prob.1
+    
+    auc  <- auc(cv.val$return, as.vector(yhat.val))[[1]]
+    loss <- helper.loss(tau_candidates = tau_candidates, 
+                        truevals       = cv.val$return, 
+                        predictedvals  = yhat.val, 
+                        itemprice      = real_price$item_price[cv.val$order_item_id])
+    sse  <- helper.sse(truevals        = cv.val$return, 
+                       predictedvals   = yhat.val)
+    res  <- list("loss"        = max(loss), 
+                 "auc"         = auc,
+                 "sse"         = min(sse), 
+                 "parameters"  = parameters[n,])
+    res
+    vec[[i]] <- res
+  }
+  results.par <- cbind(vec.1, vec)
+}
+
+
+
+# combine cross validations
+# measure <- helper.evaluate(results.par, tau_candidates)
+
+rm(k, sample.idx, train.rnd, folds, vec)
---
+++ xgboost/01-par_tuning_1ex.R
@@ -0,0 +1,100 @@
+#----------------------------------------------------------------------------------
+# parameter tuning and cross validation
+#----------------------------------------------------------------------------------
+# runs m repeated (different seed) k-fold cross validations for different 
+# tuning parameters
+# extract best settings by maximising negative loss function
+#----------------------------------------------------------------------------------
+
+library(caret)
+
+load(file = "./data/known-unknown-data.RData")
+real_price <- data.frame(known$order_item_id, known$item_price) # for loss function
+colnames(real_price) <- c("order_item_id", "item_price")
+
+# decompose dataset in pure subsets
+source(file="Decompose_Dataset.R")
+source(file = "helperfunctions.R")
+
+# settings for tuning and cross validation 
+m <- 6 # repeated Cross Validation (same algorithm but different random split)
+k <- 3 # 3-fold cross validation
+
+# Set tuning parameters
+parameters <- expand.grid("nrounds" = c(20, 100, 200),
+                         "max_depth" = c(2, 4), 
+                         "eta" = c(0.01, 0.05, 0.1, 0.15),
+                         "gamma" = 0,
+                         "colsample_bytree" = 0.8, 
+                         "min_child_weight" = 1)
+tau_candidates <- 0.002*125:425 # to calculate optimal loss
+
+#----------------------------------------------------------------------------------
+# .f       : full model, n = 25.266
+#----------------------------------------------------------------------------------
+known   <- known.f
+unknown <- unknown.f
+# additional data preparation for xgboost
+source(file = "./xgboost/00-3-xgboost_DataPrep.R") 
+known   <- known.n       # output of additional data preparation
+unknown <- unknown.n     # 
+
+# perform repeatet cross validation 
+source(file="./xgboost/00-2-rep_cv.R")
+cv.list.f <- cv.list     # store result of repeated cross validation 
+
+#----------------------------------------------------------------------------------
+# .u       : model without user_retrate, n = 92.409
+#----------------------------------------------------------------------------------
+known   <- rbind(known.f, known.u) # can also use .f for training, variables are pure
+unknown <- unknown.u               #
+# additional data preparation for xgboost
+source(file = "./xgboost/00-3-xgboost_DataPrep.R")
+known.n$user_retrate   <- NULL     # remove user_retrate (uncertain categories)
+unknown.n$user_retrate <- NULL     #
+#
+known   <- known.n                 # output of additional data preparation
+unknown <- unknown.n               #
+
+# perform repeatet cross validation
+source(file="./xgboost/00-2-rep_cv.R")
+cv.list.u <- cv.list               # store result of repeated cross validation
+
+#----------------------------------------------------------------------------------
+# .i       : model without user_retrate, n = 27.123
+#----------------------------------------------------------------------------------
+known   <- rbind(known.f, known.i) # can also use .f for training, variables are pure
+unknown <- unknown.i               #
+# additional data preparation for xgboost
+source(file = "./xgboost/00-3-xgboost_DataPrep.R")
+known.n$item_retrate   <- NULL     # remove user_retrate (uncertain categories)
+unknown.n$item_retrate <- NULL     #
+#
+known   <- known.n                 # output of additional data preparation
+unknown <- unknown.n               #
+
+# perform repeatet cross validation
+source(file="./xgboost/00-2-rep_cv.R")
+cv.list.i <- cv.list               # store result of repeated cross validation
+
+#----------------------------------------------------------------------------------
+# .iu      : model without user_retrate AND item_retrate, n = 100.000
+#----------------------------------------------------------------------------------
+known   <- rbind(known.f, known.i, known.u, known.iu)
+unknown <- unknown.i
+# additional data preparation for xgboost
+source(file = "./xgboost/00-3-xgboost_DataPrep.R")
+known.n$item_retrate   <- NULL     # remove user_retrate (uncertain categories)
+known.n$user_retrate   <- NULL     #
+unknown.n$item_retrate <- NULL     #
+unknown.n$user_retrate <- NULL     #
+#
+known   <- known.n                 # output of additional data preparation
+unknown <- unknown.n               #
+
+# perform repeatet cross validation
+source(file="./xgboost/00-2-rep_cv.R")
+cv.list.iu <- cv.list              # store result of repeated cross validation
+
+save(cv.list.f, cv.list.u, cv.list.iu, cv.list.i,
+     file = "./data/cv_lists-xgb-tuning.RData")
