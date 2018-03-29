d27bb86ce02965a6b9b587bcb4e48a48ff062eaf
FranziskaSW <franziska.wehrmann@gmail.com>
2018-03-29 22:08:48 +0300

    to calculate loss for report to compare performance of tuned models

--- submission/loss.R
+++ submission/loss.R
@@ -1,3 +1,13 @@
+#----------------------------------------------------------------------------------
+# calculates loss of tuned models (nnet, rf, xgb) for 100.000 observations
+#----------------------------------------------------------------------------------
+# neural network:  nnet
+# random forest :  rf
+# xg-boost      :  xgb
+# to compare performance of tuned models
+#----------------------------------------------------------------------------------
+
+
 source(file = "helperfunctions.R")
 
 #-----------------------------
@@ -23,44 +33,46 @@ parameters <- expand.grid("nrounds" = c(50, 100, 150),
                           "min_child_weight" = 1)
 
 
-part.loss <- function(list){
-  tau <- helper.cvlist(list)
-  x = 1:length(tau[[1]]$loss$mean)
-  df        = data.frame(tau)
+part.loss = function(list){
+  loss = helper.cvlist.tune(list)
+  x    =  1:length(loss[[1]])
+  # 3x2 plot for each tau-category
+  df        = data.frame(loss[[1]])
+  # combine tau-categories
+  # when combining the graphs from previous plot, it is important to 
+  # normalize the loss of each category to 1 since the loss depends on 
+  # the item_price, as well as the tau-category. 
+  # higher tau-category leads naturally to a higher loss
   su = (df[,1]*(-1))/max(df[,1]*(-1)) # tau-category 1
-  for (i in 2:6){                     # tau-cateogries 2:6
-    i  = (i*2)-1
-    su = su + (df[,i]*(-1))/max(df[,i]*(-1))
+  su = loss[[1]]*(-1)/max(loss[[1]]*(-1))
+  for (i in 2:6){     
+    su = su + (loss[[i]]*(-1))/max(loss[[i]]*(-1))
   }
   su = su/6 * (-1)      # normalize and change sign 
   # to compare shape to previous plots
   su2 = data.frame(su)  # change to data.frame for ggplot()
+  idx = which.max(su2[[1]])
   
-  idx <- which.max(su2[[1]])  # best settings for nnet
-  
-  loss = df[idx,1]
+  val = loss[[1]][idx]
   for (i in 2:6){
-    l = loss
-    k = (i*2)-1
-    loss = l + df[idx,k] 
+    val = val + loss[[i]][idx]
+    print(val)
   }
-  return(loss)
+  return(val)
 }
 
-f <- part.loss(cv.list.f)
+# loss of optimal settings (like in 01-tuning script) for decomposed subsets
+f  = part.loss(cv.list.f)   
+u  = part.loss(cv.list.u)
+i  = part.loss(cv.list.i)
+iu = part.loss(cv.list.iu)
 
-u <- part.loss(cv.list.u)
-
-i <- part.loss(cv.list.i)
-
-iu <- part.loss(cv.list.iu)
-
-k <- f+u+i+iu
+# sum of all subsets to obtain loss for all 100.000 observations
+k  = f + u + i + iu
 k
-k * 25000/100000
 
-# nnet 100: 1173728
-# rf   100: 1244312
-# xgb  100: 1178311
+# nnet: -1173728
+# rf  : -1244312
+# xgb : -1178311
 
 # mean of 6 runs - 3fold cv. but with 3 folds full 100.000 observations are calculated
